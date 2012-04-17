20721223e299ddef8d524108e64c41b741c157e2
cactikelly <cactikelly@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-17 06:25:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/plugnedit/trunk@532155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- PlugNedit-WP.php
+++ PlugNedit-WP.php
@@ -113,7 +113,7 @@ echo  str_replace($stringRplaceplugnedit,' ; ',$file4);
 <textarea id="plugneditreturncontent" cols="1" rows="1" style="visibility:hidden;display:none" name="plugneditreturncontent" ><?php echo stripslashes($_POST["plugneditcontent"]) ?></textarea>
 
 <script language="JavaScript">
-var Iframeview
+var Iframeview=''
 if (!document.getElementById("PlugNeditView"))
 {
 Iframeview='<iframe id="PlugNeditView" class="wp-editor-area" src="" style="z-index:100000;width:99.9%;height:500px"></iframe>'
